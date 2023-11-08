f5c73c5adaf12d2285b0ab2ace095e07402f9143
Bruno N Mota <brunonm@al.insper.edu.br>
2023-11-08 09:02:50 -0300

    teste v3

--- Termo - Python.py
+++ Termo - Python.py
@@ -1,7 +1,7 @@
 import funcoes
 
 
-print('┏━━━━━━━━━━━━━━━━━━━━━━┓\n┃  Seja bem vindo(a)!  ┃\n┃  Esse é o jogo Termo ┃\n┗━━━━━━━━━━━━━━━━━━━━━━┛')
+print('┏━━━━━━━━━━━━━━━━━━━━━━┓\n┃  Seja bem vindo(a)!  ┃\n┃  Esse é o jogo Termo oooo ┃\n┗━━━━━━━━━━━━━━━━━━━━━━┛')
 print('\n')
 print(' Regras: \n- Você tem {0} tentativas para acertar uma palavra aleatória de {1} letras.\n- A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n. Azul   : a letra está na posição correta;\n. Amarelo: a palavra tem a letra, mas está na posição errada;\n. Cinza: a palavra não tem a letra.\n- Os acentos são ignorados;\n- As palavras podem possuir letras repetidas.\n \n \n Sorteando uma palavra...\n Já tenho uma palavra! Tente adivinhá-la!\n'.format(6, 5))
 print('Você tem {0} tentativa(s)'.format(6))
