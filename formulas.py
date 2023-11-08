d9e805b3b6279d85d9b3d0757eab38f9c8edbbce
Bruno N Mota <brunonm@al.insper.edu.br>
2023-11-08 15:09:37 -0300

    organizando e entendendo as fórmulas

--- formulas.py
+++ formulas.py
@@ -3,48 +3,64 @@ from letras_e_caracteres_especiais import *
 
 
 
-
-#FUNÇÕES QUE VÃO SER USADAS NO TERMO
-def filtra(palavras, numero):
-    print(palavras)
-    normal = []
-    especiais = "?!.,"
-    
-    for palavra in palavras:
-       
-        if len(palavra) == numero:
-            corrigida = palavra.lower()
-            if not corrigida in normal:
-                normal.append(corrigida)
-    return normal
+especiais = '.;:()=+@!~/|-*><?#$%&_°}{][ªº,'
+abc = 'qwertyuiopasdfghjklzxcvbnm'
 
 #################################   
-    
-def inicializa(palavras):
+ def inicializa(palavras):
     import random
-    dic = {}
-    n = len(palavras[0])
-    dic['sorteada'] = random.choice(palavras)
-    dic["n"] = n 
-    dic['especuladas'] = []
-    dic['tentativas'] = n + 1
-    return dic
+    retorno = {}
+
+    retorno['n']=len(palavras[0]) #número de letra da palavra sorteada 
+    retorno['sorteada'] = random.choice(palavras) #seleção da palavra sorteada
+    retorno['especuladas'] = [] #adicionar a palavra especulada, se ela tiver sido aprovada (se nao foi testada e se está na lista)
+    retorno['tentativas'] = retorno['n']+1 #numero de vidas
+
+    return retorno
 
 #################################   
 
 def inidica_posicao(sorteada, especulada):
-    lista = [0]*len(sorteada)
+
+    if not len(sorteada) == len(especulada):
+        return []
     
-    if len(sorteada) != len(especulada):
-        return [] 
-    else:
-        for i in range(len(especulada)):
-            
-            if especulada[i] in sorteada:
-                lista[i] = 1
-                if especulada[i] == sorteada[i]:
-                    lista[i] = 0
-            elif especulada[i] not in sorteada:
-                lista[i] = 2
+    retorno = []
+
+    for ile in range(len(especulada)): #ile = posição da letra
+        if especulada[ile] not in sorteada: #a letra não está na sorteada
+            retorno.append(2)
+        elif especulada[ile] == sorteada[ile]: #letra no lugar certo
+            retorno.append(0)
+        
+        else:
+            retorno.append(1) #letra presente, mas no lugar errado
+    
+    return retorno        
+
+
+###############
+def filtra(palavras, ndigitos): #FILTRA PALAVRAS PELO TAMANHO, TIRA OS CARACTERES ESPECIAIS E DEVOLVE MINUSCULO
+
+    abc = 'qwertyuiopasdfghjklzxcvbnm'
+    
+    palavrasv2 = [] #criando uma lista de palavra vazia
+    for i in range(len(palavras)): #rodando a lista de palavras
+        palavrasv2.append('') 
+        #checando se há um caractere especial
+        palavras[i] = palavras[i].lower() #deixando todas as palavras minusculas
+        for letra in palavras[i]:
+            if letra in abc:  #testando se é caractere especial
+                palavrasv2[i] += letra #formando a palavra denovo
+        #a palavra já foi formada
+
+    palavrasv3 = []
+
+    for i in range(len(palavrasv2)): #rodando a nova lista de palavras
+        if len(palavrasv2[i]) == ndigitos and palavrasv2[i]not in palavrasv3: #palavra com ndigitos e não repetida
+            palavrasv3.append(palavrasv2[i])
+
+    return palavrasv3
+
                 
-    return lista
+        
