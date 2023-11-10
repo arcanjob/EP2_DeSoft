81189f0880678c3bb97973962361a360c8fda837
Bruno N Mota <brunonm@al.insper.edu.br>
2023-11-10 13:25:12 -0300

    não estou achando o erro (tabela)

--- tabela.py
+++ tabela.py
@@ -1,18 +1,27 @@
+from palavras import *
+import random
+from formulas_cruas import *
 
 info = {}
-def inicializa(palavras):
-    info['especulada'] = resposta
-    info['n']=len(palavras[0]) #número de letra da palavra sorteada 
+def inicializa(palavras): #cria o dicionário central
+    #info['especulada'] = resposta
+    info['n']= 6 #número de letra da palavra sorteada 
     info['sorteada'] = random.choice(palavras) #seleção da palavra sorteada
     info['especuladas'] = [] #adicionar a palavra especulada, se ela tiver sido aprovada (se nao foi testada e se está na lista)
     info['tentativas'] = info['n']+1 #numero de vidas
     info['sorteadas'] = [] #lista de sorteadas
     info['resultado'] = indica_posicao(info['sorteada'], info['especulada'])
     info['ntentativas'] = len(info['especuladas'])
+    info['vidas'] = info['tentativas'] - info['ntentativas']
 
     return info
 
-def tabela(n) #n = info['n'] - numero de letras
+
+inicializa(palavras)
+
+#n = info['n'] - numero de letras
+###################################CRIANDO A TABELA DINÂMICA
+def tabela(n):
     linhazinha = ' --- '*(n+1) + '\n'
     tabela = linhazinha
     linha = '|'
@@ -25,4 +34,8 @@ def tabela(n) #n = info['n'] - numero de letras
         tabela += linha + '\n' + linhazinha
         
     if info['ntentativas'] != info['tentativas']: #criando as células vazias
-        tabela += '|   '*n +'\n' 
+        tabela += '|   '*n +'\n'+ linhazinha*info['vida']
+
+    return tabela
+
+tabela(6)
